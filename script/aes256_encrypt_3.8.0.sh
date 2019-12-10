ec577d0051abcc9a8c0bfd13a6533946a3e70a9b
hakutyou <siratori@qq.com>
2019-12-10 15:01:11 +0800

    Python-3.8.0 暂无 pytorch 包 使用 3.7.5 版本

---
+++ script/aes256_encrypt_3.8.0.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# != 2 ] ; then
+        echo "USAGE: $0 project_folder password"
+        exit -1
+fi
+
+for i in `find $1 -regex .*\.py`; do
+        cp "$i" "$i.orig"
+        sed -i '1s/^\(.\)/*\1/' "$i"
+        gpg --symmetric --batch --yes --passphrase "$2" $i
+        # 替换原文件
+        mv "$i.gpg" "$i"
+        sed -i '1s/^\(.\)/*\1/' "$i"
+done
