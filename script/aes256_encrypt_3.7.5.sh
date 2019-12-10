d7e385276ab4fc4ea4807c9f2bf4cb827770ae53
hakutyou <siratori@qq.com>
2019-12-10 15:03:10 +0800

    命令统一使用双引号

--- script/aes256_encrypt_3.7.5.sh
+++ script/aes256_encrypt_3.7.5.sh
@@ -7,9 +7,9 @@ fi
 
 for i in `find $1 -regex .*\.py`; do
         cp "$i" "$i.orig"
-        sed -i '1s/^\(.\)/*\1/' "$i"
+        sed -i "1s/^/*/" "$i"
         gpg --symmetric --batch --yes --passphrase "$2" $i
         # 替换原文件
         mv "$i.gpg" "$i"
-        sed -i '1s/^\(.\)/*\1/' "$i"
+        sed -i "1s/^/*/" "$i"
 done
